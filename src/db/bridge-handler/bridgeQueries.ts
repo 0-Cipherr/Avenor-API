3688a3cc0eb407f86edfaaea52a6a2e61041c0f2
ExoticSolutions <1exoticeditz@gmail.com>
2026-09-14 13:52:02 -0400

    updates

--- src/db/bridge-handler/bridgeQueries.ts
+++ src/db/bridge-handler/bridgeQueries.ts
@@ -1,27 +1,40 @@
-import pg from "pg";
+import pg, { type QueryResult } from "pg";
 import type { BridgeQueryRules } from "../../interfaces/bridge-interfaces.js";
 import pool from "../pool.js";
+
+interface BridgeReuslt {}
 class BridgeQueries implements BridgeQueryRules {
   dbPool: pg.Pool = pool;
   constructor() {}
 
   async dbConnect(): Promise<boolean> {
+    pool.connect();
     //true indicating that connciton is successful
 
     return true;
   }
 
   async dbDisconnect(): Promise<boolean> {
+    pool.end();
     //true value means that db has been killed and dsconnected
 
     return true;
   }
 
   async postBridge() {
-    const response = await pool;
+    const result = await this.dbPool.query("");
+
+    return result.rows;
   }
 
-  async getBridge() {}
+  async getBridge(): Promise<QueryResult<any>> {
+    const response: any = await this.dbPool.query("");
 
-  async getAllBridges() {}
+    return response.rows;
+  }
+
+  async getAllBridges(): Promise<QueryResult<any>> {
+    const response: any = await this.dbPool.query("");
+    return response.rows;
+  }
 }
